d8ae47a11718ad1350e1437085568dc68584ed41
project1-ayush <trivediayush491112@gmail.com>
2021-03-27 12:48:07 +0530

    Added: Emp Present Or Absent

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome To Employee Wage Computation Program"
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
