6a1e6e4fee215adabab29b69902d275ff13a70e2
project1-ayush <trivediayush491112@gmail.com>
2021-03-27 20:00:30 +0530

    Added : Emp_Wages-

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,20 +1,25 @@
 #!/bin/bash -x
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
+numWorkingDays=20;
 empRatePerHr=20;
 
-empCheck=$((RANDOM%3));
+for((day=1; day<=$numWorkingDays ; day++))
+do
+	empCheck=$((RANDOM%3));
+				case $empCheck in 
+									$isFullTime)
+										 empHrs=8
+										 ;;
+									$isPartTime)
+										 empHrs=4
+										 ;;
+									*)
+										 empHrs=0
+										 ;;
+				esac
 
-case $empCheck in 
-					$isFullTime)
-								empHrs=8
-								;;
-					$isPartTime)
-								empHrs=4
-								;;
-					*)
-								empHrs=0
-								;;
-esac
-
-salary=$(($empRatePerHr*$empHrs));
+	salary=$(($empHrs*$empRatePerHr));
+	totalSalary=$(($totalSalary + $salary));
+done
