4f170b2e3c318b30b7346f554187d0ada98359c7
project1-ayush <trivediayush491112@gmail.com>
2021-03-27 20:15:31 +0530

    Added : Emp_Wages_For_100hours_20days

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,25 +1,36 @@
 #!/bin/bash -x
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-numWorkingDays=20;
-empRatePerHr=20;
 
-for((day=1; day<=$numWorkingDays ; day++))
+#CONSTANTS
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+#VARIABLES
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+
 do
-	empCheck=$((RANDOM%3));
-				case $empCheck in 
-									$isFullTime)
-										 empHrs=8
-										 ;;
-									$isPartTime)
-										 empHrs=4
-										 ;;
-									*)
-										 empHrs=0
-										 ;;
-				esac
+	((totalWorkingDays++))
+	empCheck=$((RANDOM % 3))
 
-	salary=$(($empHrs*$empRatePerHr));
-	totalSalary=$(($totalSalary + $salary));
+	case $empCheck in
+			$IS_FULL_TIME)
+				empHrs=8
+				;;
+			$IS_PART_TIME)
+				empHrs=4
+				;;
+			*)
+				empHrs=0
+				;;
+	esac
+
+
+	totalEmpHrs=$(($totalEmpHrs + $empHrs))
 done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
