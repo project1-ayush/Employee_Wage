e9fbdb4bb1342b50598e5cbfa7be824cec5a1bfa
project1-ayush <trivediayush491112@gmail.com>
2021-03-27 14:18:22 +0530

    Added: Employee Wage Using Case

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,21 +1,20 @@
 #!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
 
-echo "Welcome to Employee Wage Computation Program"
+empCheck=$((RANDOM%3));
 
-isPartTime=1
-isFullTime=2
-empRatePerHr=20
-randomCheck=$((RANDOM%3))
+case $empCheck in 
+					$isFullTime)
+								empHrs=8
+								;;
+					$isPartTime)
+								empHrs=4
+								;;
+					*)
+								empHrs=0
+								;;
+esac
 
-if [ $isFullTime -eq $randomCheck ]
-then
-		empHrs=8;
-elif [ $isPartTime -eq $randomCheck ]
-then
-		empHrs=4;
-else
-		empHrs=0;
-fi
-
-salary=$(($empHrs*$empRatePerHr))
-
+salary=$(($empRatePerHr*$empHrs));
